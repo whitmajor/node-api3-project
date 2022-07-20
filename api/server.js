42d5fb4d60ed0f791f6425c4d09b34fff650f5f7
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-19 21:20:18 -0400

    added codegrade

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
-const server = express();
+const {logger}= require("./middleware/middleware")
+
+const server = express()
+const serverRouter= require("./users/users-router")
 
 // remember express by default cannot parse JSON in request bodies
 
-// global middlewares and the user's router need to be connected here
+// global middleware and the user's router need to be connected here
+
+server.use(express.json())
+
+server.use(logger)
+server.use(serverRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
